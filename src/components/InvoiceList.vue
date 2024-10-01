57546f23de7cc61e312030526b3e40610616ecfa
Yulianto Tri Handono <yulianto.nail@gmail.com>
2024-10-02 03:33:24 +0700

    readme

--- src/components/InvoiceList.vue
+++ src/components/InvoiceList.vue
@@ -29,6 +29,8 @@
           <p>Whatsapp : <a href="wa.me/6285179918528">Lian / Yulianto Tri</a></p>
           <p>Github : <a href="https://github.com/yorunairu/crud-sandbox-tripay">Yoru Nairu</a></p>
           <p>Email : <a href="yorunairu@gmail.com">yorunairu@gmail.com</a></p>
+          <p>Demo Vid : <a href="https://drive.google.com/file/d/1MSil3SDq-GVMMlfHBVxiFaj3vjVc6Fpd/view?usp=sharing">GDrive</a></p>
+          <p>Demo Vercel (error CORS) : <a href="https://crud-tripay.vercel.app/">Vercel</a></p>
           <small>untuk running dev, menggunakan vercel. karena saat npm run dev (local) atau di vercel prod deployment error CORS</small>
         </div>
       </div>
